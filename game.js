3eb32c65cee658c677e9718086405619559442d4
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-07-29 15:42:05 +0500

    Create screen controller function

--- game.js
+++ game.js
@@ -8,7 +8,6 @@ function createPlayer(name,mark,gameBoard)
     
     let choice=0
     const playTurn=()=>{
-        choice=prompt(`Turn of ${name}`);
         if(gameBoard.row[choice-1]==="")
             gameBoard.row[choice-1]=mark;
         else
@@ -25,37 +24,42 @@ function playGame()
     let gameBoard=createGameBoard();
     const player1=createPlayer("Uzair","x",gameBoard);
     const player2=createPlayer("Yahya","o",gameBoard);
-    while(result===0)
+    let getResult=()=>{
+        if((        gameBoard.row[0]===gameBoard.row[1] && gameBoard.row[0]===gameBoard.row[2] && gameBoard.row[0]!=="")
+                || (gameBoard.row[3]===gameBoard.row[4] && gameBoard.row[3]===gameBoard.row[5] && gameBoard.row[3]!=="")
+                || (gameBoard.row[6]===gameBoard.row[7] && gameBoard.row[6]===gameBoard.row[8] && gameBoard.row[6]!=="")
+
+                || (gameBoard.row[0]===gameBoard.row[3] && gameBoard.row[0]===gameBoard.row[6] && gameBoard.row[0]!=="")
+                || (gameBoard.row[1]===gameBoard.row[4] && gameBoard.row[1]===gameBoard.row[7] && gameBoard.row[1]!=="")
+                || (gameBoard.row[2]===gameBoard.row[5] && gameBoard.row[2]===gameBoard.row[8] && gameBoard.row[2]!=="")
+
+                || (gameBoard.row[0]===gameBoard.row[4] && gameBoard.row[0]===gameBoard.row[8] && gameBoard.row[0]!=="")
+                || (gameBoard.row[2]===gameBoard.row[4] && gameBoard.row[2]===gameBoard.row[6] && gameBoard.row[2]!=="")
+                )
+            return 1;
+        else
+            return 0;
+    };
+    function playerTurn()
     {
-        let getResult=()=>{
-            if((        gameBoard.row[0]===gameBoard.row[1] && gameBoard.row[0]===gameBoard.row[2] && gameBoard.row[0]!=="")
-                    || (gameBoard.row[3]===gameBoard.row[4] && gameBoard.row[3]===gameBoard.row[5] && gameBoard.row[3]!=="")
-                    || (gameBoard.row[6]===gameBoard.row[7] && gameBoard.row[6]===gameBoard.row[8] && gameBoard.row[6]!=="")
-
-                    || (gameBoard.row[0]===gameBoard.row[3] && gameBoard.row[0]===gameBoard.row[6] && gameBoard.row[0]!=="")
-                    || (gameBoard.row[1]===gameBoard.row[4] && gameBoard.row[1]===gameBoard.row[7] && gameBoard.row[1]!=="")
-                    || (gameBoard.row[2]===gameBoard.row[5] && gameBoard.row[2]===gameBoard.row[8] && gameBoard.row[2]!=="")
-
-                    || (gameBoard.row[0]===gameBoard.row[4] && gameBoard.row[0]===gameBoard.row[8] && gameBoard.row[0]!=="")
-                    || (gameBoard.row[2]===gameBoard.row[4] && gameBoard.row[2]===gameBoard.row[6] && gameBoard.row[2]!=="")
-                    )
-                return 1;
-            else
-                return 0;
-        };
         player1.playTurn();
         result=getResult();
-        if (result!==0)
-            break;
-        console.log(player1.getTurn());
+        console.log("playerTurn "+player1.getTurn());
         player2.playTurn();
         result=getResult();
         if (result!==0)
-            break;
-        console.log(player2.getTurn());
+        console.log("playerTurn "+player2.getTurn());
     }
-    return{gameBoard};
+    return{gameBoard,player1,player2,result,getResult};
     
-}
-let start=playGame();
-console.log(start.gameBoard);
+};
+
+function screenController()
+{
+    const playerSelectionBox=document.querySelectorAll("a");
+    let start=playGame();
+    console.log(start.gameBoard);
+    console.log(start.player1.name);
+};
+
+screenController();
